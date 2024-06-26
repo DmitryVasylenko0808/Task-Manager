963e7e482f5b86fa225ce3bfcd15c51c444f1909
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-06-26 19:22:09 +0300

    No board page

--- client/src/pages/NoBoardPage.tsx
+++ client/src/pages/NoBoardPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NoBoardPage = () => {
-  return <div>NoBoardPage</div>;
+  return (
+    <div className="flex justify-center items-center bg-tm-gray h-full">
+      <span className="text-2xl text-tm-black-100">No board selected</span>
+    </div>
+  );
 };
 
 export default NoBoardPage;
